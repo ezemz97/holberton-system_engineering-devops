90919e5b1855235655e855ba2d94c0c7e46c7250
Eze <ezedksl@gmail.com>
2021-08-13 23:14:06 -0300

    added tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Creates a file in /tmp with 0744 permissions
+file { '/tmp/holberton':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+  }
