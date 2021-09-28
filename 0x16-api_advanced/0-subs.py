e673575b0dcaed14216963183286606673dda29d
Eze <ezedksl@gmail.com>
2021-09-28 15:48:34 +0000

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Return the number of suscribers for a given subreddit - Reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    r = subreddit
+    data = requests.get("https://www.reddit.com/r/{}/about.json".format(r),
+                        headers={'User-agent': 'Pito'},
+                        allow_redirects=False)
+
+    if data.status_code == 200:
+        return data['data']['subscribers']
+    return 0
